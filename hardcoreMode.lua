fcc31a3bc574fde421c3153072274f19421a7f27
0x028a <89611822+0x028a@users.noreply.github.com>
2021-09-01 15:58:44 -0400

    Upload Main File
    added file

---
+++ hardcoreMode.lua
@@ -0,0 +1,40 @@
+--Item that designates that character is a hardcore character.
+local hardCoreItem = 90000
+local hcNPC = 90000
+
+--on death function - checks if player has token and bans character if it does.
+-- 99999999
+local function PlayerDeath(event, killer, killed)
+    if(killed:HasItem(hardCoreItem,1)) then
+        print(killed:GetName() .. " was killed by " .. killer:GetName())
+        SendWorldMessage(killed:GetName() .. " was killed by " .. killer:GetName())
+        Ban(1,killed:GetName(),1)
+    end
+end
+
+function OnFirstTalk(event, player, unit)
+	player:GossipMenuAddItem(0, "Looking for a challenge??? Click here to try hardcore mode!", 0, 1)
+	player:GossipSendMenu(1, unit)
+end
+
+function OnSelect(event, player, unit, sender, intid, code)
+	if (intid == 1) then
+		player:GossipMenuAddItem(0, "Just making sure that you want to turn on hardcore mode?? This will lock the character after death and you will no longer be able to play the character!!!", 0, 2)
+        player:GossipMenuAddItem(0, "NO TAKE ME BACK!", 0, 3)
+        player:GossipSendMenu(2, unit)
+	end
+end
+
+function OnHardCore(event, player, unit, sender, initid, code)
+    if (initd == 2) then
+        player:GiveItem(90000)
+    else
+        player:GossipComplete()
+    end
+end
+
+
+RegisterCreatureGossipEvent(hcNPC, 1 , OnFirstTalk)
+RegisterCreatureGossipEvent(hcNPC, 2, OnSelect)
+RegisterCreatureGossipEvent(hcNPC, 2, OnHardCore)
+RegisterPlayerEvent( 8, PlayerDeath)
